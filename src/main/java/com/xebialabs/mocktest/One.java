3d6e416997d294f0cd168af15a848493d107ba58
Jesse van Bekkum <jvanbekkum@xebia.com>
2015-03-26 16:12:34 +0100

    Made tests a bit slower

--- src/main/java/com/xebialabs/mocktest/One.java
+++ src/main/java/com/xebialabs/mocktest/One.java
@@ -1,6 +1,7 @@
 package com.xebialabs.mocktest;
 
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 public class One {
     private int counter = 0;
@@ -15,14 +16,29 @@ public class One {
     }
 
     public boolean fails() {
+        try {
+            TimeUnit.MILLISECONDS.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return false;
     }
 
     public boolean succeeds() {
+        try {
+            TimeUnit.MILLISECONDS.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return true;
     }
 
     public boolean random(double probability) {
+        try {
+            TimeUnit.MILLISECONDS.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return random.nextDouble() < probability;
     }
 
